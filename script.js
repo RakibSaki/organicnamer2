9e998438fe0dfa8ccfceae154ba4cab8b88f1d0e
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-04-26 22:15:49 +0600

    Try to name molecule

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ class Atom {
             let width = Math.sqrt(((this.x - bondedTo.x) * (this.x - bondedTo.x)) + ((this.y - bondedTo.y) * (this.y - bondedTo.y)))
             bond.style.width = `${width}px`
             let rotate = Math.atan((bondedTo.y - this.y) / (bondedTo.x - this.x))
-            console.log(rotate)
             bond.style.transform = `translate(${width / -2}px, -1px) rotate(${rotate}rad)`
             // bond.style.transform = `rotate(${rotate}rad) translate(${0 * Math.abs((this.x - bondedTo.x) / 2)}px, ${-1 * Math.abs((this.y - bondedTo.y) / 2)}px)`
             // bond.style.transform = `rotate(${rotate}rad) translate(${-1 * Math.abs((this.x - bondedTo.x) / 2)}px, ${-1 * Math.abs((this.y - bondedTo.y) / 2)}px)`
@@ -62,6 +61,13 @@ class Molecule {
     }
 }
 
+class Chain {
+    constructor(atoms) {
+        this.atoms = atoms
+        this.branchesAt = []
+    }
+}
+
 let atoms = []
 let molecules = []
 let cursorInAtom = false
@@ -76,6 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 atom.style.backgroundColor = 'white'
             }
             atoms.push(newAtom)
+            console.log(nameMolecule(newAtom.molecule))
             if (!selected) {
                 molecules.push(newAtom.molecule)
             }
@@ -83,3 +90,55 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
+
+function nameMolecule(molecule) {
+    let chains = []
+    for (let moleculeAtomIndex = 0; moleculeAtomIndex < molecule.atoms.length; moleculeAtomIndex++) {
+        let atom = molecule.atoms[moleculeAtomIndex]
+        if (atom.bonds.length == 0) {
+            chains.push(new Chain([atom]))
+        }
+        if (atom.bonds.length == 1) {
+            let newChain = new Chain([atom, atom.bonds[0]])
+            chains.push(newChain)
+            let changed = false
+            do {
+                changed = checkChains(chains)
+                console.log('checking')
+            } while (changed)
+        }
+    }
+    let parentLength = 0
+    for (let chain of chains) {
+        if (chain.atoms.length > parentLength) {
+            parentLength = chain.atoms.length
+        }
+    }
+    console.log(chains)
+    return parentLength
+}
+
+function checkChains(chains) {
+    let changed = false
+    for (let chainIndex = 0; chainIndex < chains.length; chainIndex++) {
+        let chain = chains[chainIndex]
+        let lastAtom = chain.atoms[chain.atoms.length - 1]
+        if (lastAtom.bonds > 1) {
+            changed = true
+            let secondlastAtom = chain.atoms[chain.atoms.length - 2]
+            let otherAtoms = []
+            for (let i = 0; i < lastAtom.bonds.length; i++) {
+                if (lastAtom.bonds[i] != secondlastAtom) {
+                    otherAtoms.push(lastAtom.bonds[i])
+                }
+            }
+            for (let i = 1; i < otherAtoms.length; i++) {
+                let newChain = chain.copy()
+                newChain.atoms.push(otherAtoms[i])
+                chains.push(newChain)
+            }
+            chain.atoms.push(otherAtoms[0])
+        }
+    }
+    return changed
+}
